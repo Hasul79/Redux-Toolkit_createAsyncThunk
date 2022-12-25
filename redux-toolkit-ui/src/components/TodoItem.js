3e8cea03de576776d657ad87bfbe81f293069789
Hasul79 <hatuk2@mail.ru>
2022-12-25 23:57:05 +0400

    create removeTodo

--- redux-toolkit-ui/src/components/TodoItem.js
+++ redux-toolkit-ui/src/components/TodoItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {useDispatch} from 'react-redux';
-import {toggleCompletedTodo} from '../features/todo/todoSlice'
+import {toggleCompletedTodo, removeTodo} from '../features/todo/todoSlice'
 
 const TodoItem = ({todo}) => {
   const dispatch = useDispatch()
 
   const toggleTodoHandler = (id) => {
      dispatch(toggleCompletedTodo(id))
+  }
+
+  const removeTodoHandler = (id) => {
+    dispatch(removeTodo(id))
   }
     return (
         <div className='flex justify-between items-center my-2'>
@@ -17,7 +21,8 @@ const TodoItem = ({todo}) => {
             <div className={`text-sm ${todo.completed ? 'line-through font-medium text-lime-400' : ''}`}>
                 {todo.text}
             </div>
-            <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
+            <div onClick={() => removeTodoHandler(todo.id)} 
+                className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
                 Delete
             </div>
         </div>
--- redux-toolkit-ui/src/features/todo/todoSlice.js
+++ redux-toolkit-ui/src/features/todo/todoSlice.js
@@ -14,9 +14,12 @@ export const todoSlice = createSlice({
         toggleCompletedTodo: (state, action) => {
            const toggleTodo = state.todos.find((todo) => todo.id === action.payload)
            toggleTodo.completed = !toggleTodo.completed
-        },  
+        }, 
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        } 
     },
 })
 
-export const {addTodo , toggleCompletedTodo} = todoSlice.actions
+export const {addTodo , toggleCompletedTodo, removeTodo} = todoSlice.actions
 export default todoSlice.reducer
